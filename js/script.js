15300b7c90c6adcfa4a6ecaae2a1db034db82858
Carlos <cgomessilva2015@gmail.com>
2022-09-12 22:51:45 -0300

    parametros url

--- js/script.js
+++ js/script.js
@@ -16,7 +16,10 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-  console.log(parametro);
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
 parametros.forEach(ativarProduto);
