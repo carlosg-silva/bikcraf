161b9990bd2c6d4c49a488cda1b7941ae094dc41
Carlos <cgomessilva2015@gmail.com>
2022-09-13 13:56:31 -0300

    galeria imagens js

--- js/script.js
+++ js/script.js
@@ -43,4 +43,21 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
-console.log(perguntas);
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
